e6b74bb00013811ceaf0bba0d17d0952b068bc9e
recreant12 <victor.shokuroff@gmail.com>
2021-01-06 22:22:58 -0600

    Setup Sauce Labs

--- src/test/java/com/romanceabroad/ui/BaseUI.java
+++ src/test/java/com/romanceabroad/ui/BaseUI.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -13,6 +15,8 @@ import org.testng.annotations.Parameters;
 import org.testng.asserts.SoftAssert;
 
 import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class BaseUI {
@@ -30,50 +34,68 @@ public class BaseUI {
     ContactUsPage contactUsPage;
     String mainUrl = "https://romanceabroad.com/";
     SoftAssert softAssert = new SoftAssert();
+    
+    protected TestBox testBox ;
+
+    protected enum TestBox {
+        LOCAL, SAUCE
+    };
 
 
     @BeforeMethod(groups = {"user", "admin"}, alwaysRun = true)
+    @Parameters({"browser", "version", "platform", "testbox"})
 
-    @Parameters("browser")
-
-    public void setup(@Optional("chrome") String browser, Method method){
+    public void setup(@Optional("chrome") String browser,@Optional("null") String version,@Optional("null") String platform,@Optional("null") String box, Method method) throws MalformedURLException {
 
         Reports.start(method.getDeclaringClass().getName() + " : " + method.getName());
 
-        // Check if parameter passed from TestNG is 'firefox'
-
-        if (browser.equalsIgnoreCase("firefox")) {
-
-            // Create firefox instance
-
-            System.setProperty("webdriver.gecko.driver", "geckodriver");
-
-            driver = new FirefoxDriver();
-
+        if(box.equalsIgnoreCase("local")) {
+            testBox = TestBox.LOCAL;
+        } else if(box.equalsIgnoreCase("sauce")) {
+            testBox = TestBox.SAUCE;
         }
 
-        // Check if parameter passed as 'chrome'
+        switch (testBox) {
+            case LOCAL:
+            // Check if parameter passed from TestNG is 'firefox'
+            if (browser.equalsIgnoreCase("firefox")) {
+                // Create firefox instance
+                System.setProperty("webdriver.gecko.driver", "geckodriver");
+                driver = new FirefoxDriver();
+            }
 
-        else if (browser.equalsIgnoreCase("chrome")) {
+            // Check if parameter passed as 'chrome'
 
-            // Set path to chromedriver.exe
+            else if (browser.equalsIgnoreCase("chrome")) {
+                // Set path to chromedriver.exe
+                System.setProperty("webdriver.chrome.driver", "chromedriver");
+                // Create chrome instance
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--kiosk");
+                driver = new ChromeDriver(options);
+                driver.get("chrome://settings/clearBrowserData");
 
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
+            } else {
+                System.setProperty("webdriver.chrome.driver", "chromedriver");
 
-            // Create chrome instance
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--kiosk");
-            driver = new ChromeDriver(options);
-            driver.get("chrome://settings/clearBrowserData");
-
-        }
-        else {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--kiosk");
-            driver = new ChromeDriver(options);
-            driver.get("chrome://settings/clearBrowserData");
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--kiosk");
+                driver = new ChromeDriver(options);
+                driver.get("chrome://settings/clearBrowserData");
+            }
+            break;
+            case SAUCE:
+                DesiredCapabilities capabilities = new DesiredCapabilities();
+                capabilities.setCapability("username", "recreant");
+                capabilities.setCapability("accessKey", "9789592c-10d1-4d12-8055-c861fc5bc38d");
+                capabilities.setCapability("browserName", browser);
+                capabilities.setCapability("platform", platform);
+                capabilities.setCapability("version", version);
+                capabilities.setCapability("name", method.getName());
+                driver = new RemoteWebDriver(
+                        new URL("https//" + System.getenv("SAUCE_USERNAME") + ":" + System.getenv("SAUCE_ACCESS_KEY") + "@ondemand.us-west-1.saucelabs.com:443/wd/hub"),
+                        capabilities);
+                break;
         }
         wait = new WebDriverWait(driver, 30);
         mainPage = new MainPage(driver, wait);
